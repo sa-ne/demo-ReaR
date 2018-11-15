f8a2765819e62f3a61f4639194c9160833d6ced7
Mark Tonneson <mtonneso@mtonneso.remote.csb>
2018-11-15 14:20:16 -0500

    Updates for readability

--- rear-cleanup.sh
+++ rear-cleanup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Clean up all demo changes
-echo -e "Cleaning up changes made by demo ..."
+echo -e "
+Cleaning up changes made by demo ...
+"
+
 yum remove -y rear genisoimage syslinux
 rm -rf /tmp/rescue_system
 rm -rf /etc/rear
@@ -10,3 +13,4 @@ rm -rf /var/log/rear
 mount -t nfs gateway:/home/mtonneso/Downloads /mnt/tmp
 rm -rf /mnt/tmp/demos
 rm -f /mnt/tmp/rear-demos.iso
+umount /mnt/tmp
